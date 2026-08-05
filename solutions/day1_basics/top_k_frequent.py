8aff699ec541802e28a0611263e783d4d21f916e
officialbidisha1 <officialbidisha1@gmail.com>
2026-08-06 01:21:37 +0530

    feat:added top k frequent by bucket sort

--- solutions/day1_basics/top_k_frequent.py
+++ solutions/day1_basics/top_k_frequent.py
@@ -3,7 +3,24 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
+        freq = {}
+        buckets= []
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1 # 2-> 1 (2 is present once)
+        for i in range(len(nums)+1):
+            buckets.append([])
+        for key,value in freq.items():
+            buckets[value].append(key)
+        res = []
+        # traverse the array from the end
+        #start, stop, step
+        for count in range(len(nums), 0, -1):
+            for num in buckets[count]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+        return res
+        
 
 
 if __name__ == "__main__":
